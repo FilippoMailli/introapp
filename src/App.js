7a68c8a7d4051a6894673ee9d775ffc18bbee675
Filippo Mailli <filippo.mailli@gmail.com>
2020-12-20 19:01:28 +0100

    RenderApp

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import {Component} from "react/cjs/react.production.min";
+import React, {Component} from 'react';
+// import Hello from './hello.js';
+
+const happy = <h2>I am Happy</h2>;
+    function getDate(date) {
+        return date.toLocaleDateString() + ' ' + date.toLocaleTimeString()
+    }
 
 class App extends Component {
   render() {
     return (
+        <React.Fragment>
         <div className="App">
-          <header className="App-header">
-              <h1>Hello World!</h1>
-              <p></p>
-          </header>
+            <h1>My first React App</h1>
+          {/*<header className="App-header">*/}
+              {/*<Hello/>*/}
+          {/*</header>*/}
         </div>
+            {happy}
+            { 4 + 4}
+            <h2> Today is { new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString() }</h2>
+            <h2> { getDate(new Date())}</h2>
+        </React.Fragment>
     );
   }
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+function renderApp(){
+    ReactDOM.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+setInterval(renderApp, 1000);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
